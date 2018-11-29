18b0a53b9d8012f13fff53dc9b766921971b36bc
sonicskye <sonic_skye@yahoo.com>
2018-11-30 07:41:12 +1100

    basic functions

---
+++ vars.py
@@ -0,0 +1,19 @@
+# the name of the website
+govName = "lampungprov"
+
+# the static code is the last 3 numbers in Kode
+# see https://lpse.tulangbawangbaratkab.go.id/eproc4/lelang
+staticCode = "121"
+
+# two numbers are required
+# lowNum is the lowest number
+# highNum is the highest number to query
+lowNum = 7300
+highNum = 7400
+
+frontURL = "https://lpse." + govName + ".go.id/eproc4/lelang/"
+pengumumanLelangURL = "/pengumumanlelang"
+pesertaURL = "/peserta"
+hasilURL = "/hasil"
+pemenangURL = "/pemenang"
+pemenangBerkontrakURL = "/pemenangberkontrak"
